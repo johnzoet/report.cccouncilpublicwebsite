2a5c2624a865987217c47778ad27795b0149ce61
John Zoetebier <john@zoetebier.net>
2022-02-08 19:44:09 +1300

    Added debugging file

---
+++ backend/send_report_debug_example.py
@@ -0,0 +1,44 @@
+import boto3
+import os
+import base64
+import re
+
+
+dynamodb_client = boto3.client('dynamodb')
+
+def handler(event, context):
+    
+    encodedStr = event['body']
+    print("This is a debug print test")
+    print("=== BEGIN")
+    print(encodedStr)
+    print("=== END")
+    
+
+    b64decodeUTF8Str = base64.b64decode(encodedStr).decode('utf-8')
+
+    b64decodeUTF8StrStripped = b64decodeUTF8Str.replace("\n", "")
+    b64decodeUTF8StrStripped = b64decodeUTF8StrStripped.replace("\r","")
+
+    matches = re.findall(r'name=\".*?\"(.*?)-', b64decodeUTF8StrStripped)
+
+    email = matches[0]
+    issue = matches[1]
+#    email = "test1@company.com"
+#    issue = "Issue 1"
+
+    try:
+        dynamodb_client.put_item (TableName=os.environ['DYNAMODB_TABLE'], Item={'reporterEmail': {'S': email}, 'reporterIssue': {'S': issue}})
+
+        response = {
+            "statusCode": 200,
+            "headers": {'Access-Control-Allow-Origin': '*'}
+        }
+
+    except:
+        response = {
+            "statusCode": 500,
+            "headers": {'Access-Control-Allow-Origin': '*'}
+            }
+
+    return response
